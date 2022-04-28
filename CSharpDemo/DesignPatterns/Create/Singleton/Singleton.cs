a06a128567601a3f267204fb2a12afc01a67300c
fengjuanchensha <1130217572@qq.com>
2022-04-28 18:44:43 +0800

    单例模式
    添加单例模式

---
+++ CSharpDemo/DesignPatterns/Create/Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDemo.DesignPatterns.Create.Singleton
+{
+    internal class Singleton
+    {
+        public readonly static object SingletonLock = new object();
+        protected Singleton()
+        {
+
+        }
+        private Singleton instance;
+
+        public Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (SingletonLock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+    }
+}
