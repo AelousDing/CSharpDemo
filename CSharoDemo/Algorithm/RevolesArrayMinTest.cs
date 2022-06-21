56239a1b9442cf6fceeb81d49135ae26536770c3
fengjuanchensha <1130217572@qq.com>
2022-06-21 17:49:11 +0800

    添加数组旋转最小值查找
    添加数组旋转最小值查找

---
+++ CSharoDemo/Algorithm/RevolesArrayMinTest.cs
@@ -0,0 +1,23 @@
+﻿using CSharpDemo.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharoDemo.Algorithm
+{
+    [TestClass]
+    public class RevolesArrayMinTest
+    {
+        int[] input = new int[] { 3, 4, 5, 1, 2 };
+        [TestMethod]
+        public void Sum()
+        {
+            RevolesArrayMin revolesArrayMin = new RevolesArrayMin();
+            int min = revolesArrayMin.Min(input);
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ CSharpDemo/Algorithm/RevolesArrayMin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    public class RevolesArrayMin
+    {
+        /// <summary>
+        /// arr 是升序旋转的数组
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public int Min(int[] arr)
+        {
+            if (arr == null || arr.Count() < 2)
+            {
+                throw new ArgumentException("arr");
+            }
+            int left = 0;
+            int right = arr.Length - 1;
+            int mid = left;
+            while (arr[left] >= arr[right])
+            {
+                if (right - left == 1)
+                {
+                    break;
+                }
+                mid = (left + right) / 2;
+
+                if (arr[left] == arr[mid] && arr[mid] == arr[right])
+                {
+                    return MinInOrder(arr, left, right);
+                }
+
+                if (arr[left] <= arr[mid])
+                {
+                    left = mid;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return arr[right];
+        }
+
+        private int MinInOrder(int[] arr, int left, int right)
+        {
+            int result = arr[left];
+            for (int i = left + 1; i < right; i++)
+            {
+                if (result > arr[i])
+                {
+                    result = arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
