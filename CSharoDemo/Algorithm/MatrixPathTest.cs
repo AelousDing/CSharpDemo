a659b76ee7dc98707fe2faa6aae568795b183d5b
fengjuanchensha <1130217572@qq.com>
2022-06-25 11:24:23 +0800

    添加矩阵中寻找字符串算法
    添加矩阵中寻找字符串算法

---
+++ CSharoDemo/Algorithm/MatrixPathTest.cs
@@ -0,0 +1,31 @@
+﻿using CSharpDemo.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharoDemo.Algorithm
+{
+    [TestClass]
+    public class MatrixPathTest
+    {
+        char[][] matrix = new char[3][];
+
+        [TestInitialize]
+        public void Init()
+        {
+            matrix[0] = new char[] { 'a', 'b', 't', 'g' };
+            matrix[1] = new char[] { 'c', 'f', 'c', 's' };
+            matrix[2] = new char[] { 'j', 'd', 'e', 'h' };
+        }
+        [TestMethod]
+        public void HasPath()
+        {
+            MatrixPath matrixPath = new MatrixPath();
+            var hasPath = matrixPath.HasPath(matrix, 3, 4, "bfce");
+            Assert.IsTrue(hasPath);
+        }
+    }
+}
---
+++ CSharpDemo/Algorithm/MatrixPath.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    public class MatrixPath
+    {
+        public bool HasPath(char[][] matrix, int rows, int colums, string str)
+        {
+            if (matrix == null || rows < 1 || colums < 1 || string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            bool[][] visited = new bool[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                visited[i] = new bool[colums];
+                for (int j = 0; j < colums; j++)
+                {
+                    visited[i][j] = false;
+                }
+            }
+            int pathLength = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    if (HasPathCore(matrix, rows, colums, i, j, pathLength, str, visited))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private bool HasPathCore(char[][] matrix, int rows, int colums, int row, int column, int pathLength, string str, bool[][] visited)
+        {
+            if (str.Count() - 1 == pathLength)
+            {
+                return true;
+            }
+            bool hasPath = false;
+            if (row >= 0 && row < rows
+                && column >= 0 && column < colums
+                && matrix[row][column] == str[pathLength]
+                && !visited[row][column])
+            {
+                pathLength++;
+                visited[row][column] = true;
+
+                hasPath = HasPathCore(matrix, rows, colums, row, column - 1, pathLength, str, visited)
+                    || HasPathCore(matrix, rows, colums, row - 1, column, pathLength, str, visited)
+                    || HasPathCore(matrix, rows, colums, row, column + 1, pathLength, str, visited)
+                    || HasPathCore(matrix, rows, colums, row + 1, column, pathLength, str, visited);
+                if (!hasPath)
+                {
+                    pathLength--;
+                    visited[row][column] = false;
+                }
+            }
+            return hasPath;
+        }
+    }
+}
