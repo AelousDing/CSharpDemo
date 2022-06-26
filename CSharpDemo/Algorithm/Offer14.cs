6f0e74bb84f4756cc43eda891a3773fb2a1ffba2
fengjuanchensha <1130217572@qq.com>
2022-06-26 18:30:52 +0800

    添加剑指offer面试题14
    添加剑指offer面试题14

---
+++ CSharpDemo/Algorithm/Offer14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    /// <summary>
+    /// 剪绳子
+    /// </summary>
+    public class Offer14
+    {
+        #region 动态规划方法
+        public int MaxMultiple(int length)
+        {
+
+        }
+        #endregion
+
+        #region 贪婪方法
+
+        #endregion
+    }
+}
