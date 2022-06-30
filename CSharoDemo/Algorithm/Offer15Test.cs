4ad1882a2c9c86e5ef576c225d6fced65e18ed22
aelous <1130217572@qq.com>
2022-06-30 18:02:07 +0800

    offer15
    offer15

---
+++ CSharoDemo/Algorithm/Offer15Test.cs
@@ -0,0 +1,22 @@
+﻿using CSharpDemo.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharoDemo.Algorithm
+{
+    [TestClass]
+    public class Offer15Test
+    {
+        [TestMethod]
+        public void NumberOf1_IsOk()
+        {
+            Offer15 offer15 = new Offer15();
+            int count = offer15.NumberOf1(10);//10 二进制1010
+            Assert.AreEqual(2, count);
+        }
+    }
+}
---
+++ CSharpDemo/Algorithm/Offer15.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    public class Offer15
+    {
+        public int NumberOf1(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                count++;
+                n = n & (n - 1);
+            }
+            return count;
+        }
+    }
+}
