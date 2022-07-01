fe6fbd78dbea13ed9288e97330c67b682b7eafe0
aelous <1130217572@qq.com>
2022-07-01 17:39:21 +0800

    offer17
    offer17

---
+++ CSharoDemo/Algorithm/Offer17Test.cs
@@ -0,0 +1,33 @@
+﻿using CSharpDemo.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharoDemo.Algorithm
+{
+    [TestClass]
+    public class Offer17Test
+    {
+        [TestMethod]
+        public void Print1ToMaxOfNDigits_Positive()
+        {
+            Offer17 offer17 = new Offer17();
+            offer17.Print1ToMaxOfNDigits(2);
+        }
+        [TestMethod]
+        public void Print1ToMaxOfNDigits_Nagetive()
+        {
+            Offer17 offer17 = new Offer17();
+            offer17.Print1ToMaxOfNDigits(-2);
+        }
+        [TestMethod]
+        public void Print1ToMaxOfNDigits_0()
+        {
+            Offer17 offer17 = new Offer17();
+            offer17.Print1ToMaxOfNDigits(0);
+        }
+    }
+}
---
+++ CSharpDemo/Algorithm/Offer17.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    public class Offer17
+    {
+        public void Print1ToMaxOfNDigits(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+            char[] number = new char[n];
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = '0';
+            }
+            while (!Increment(number))
+            {
+                PrintNumber(number);
+            }
+        }
+
+        private void PrintNumber(char[] number)
+        {
+            bool isBeginning0 = true;
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (isBeginning0 && number[i] != '0')
+                {
+                    isBeginning0 = false;
+                }
+                if (!isBeginning0)
+                {
+                    Console.Write(number[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private bool Increment(char[] number)
+        {
+            bool isOverflow = false;
+            int nTakeOver = 0;
+            int length = number.Length;
+            int sum = 0;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                sum = number[i] - '0' + nTakeOver;
+                if (i == length - 1)
+                {
+                    sum++;
+                }
+                if (sum >= 10)
+                {
+                    if (i == 0)
+                    {
+                        isOverflow = true;
+                    }
+                    else
+                    {
+                        nTakeOver = 1;
+                        sum -= 10;
+                        number[i] = (char)(sum + '0');
+                    }
+                }
+                else
+                {
+                    number[i] = (char)(sum + '0');
+                    break;
+                }
+            }
+            return isOverflow;
+        }
+    }
+}
