25dd127554751bdf59fcf1ea369453462aa5f637
aelous <1130217572@qq.com>
2022-07-01 16:05:41 +0800

    offer16
    offer16

---
+++ CSharoDemo/Algorithm/Offer16Test.cs
@@ -0,0 +1,36 @@
+﻿using CSharpDemo.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharoDemo.Algorithm
+{
+    [TestClass]
+    public class Offer16Test
+    {
+        [TestMethod]
+        public void Power_Positive()
+        {
+            Offer16 offer16 = new Offer16();
+            double result = offer16.Power(2, 2);
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void Power_0()
+        {
+            Offer16 offer16 = new Offer16();
+            double result = offer16.Power(2, 0);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void Power_Negative()
+        {
+            Offer16 offer16 = new Offer16();
+            double result = offer16.Power(2, -2);
+            Assert.AreEqual(0.25, result);
+        }
+    }
+}
---
+++ CSharpDemo/Algorithm/Offer16.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo.Algorithm
+{
+    public class Offer16
+    {
+        public bool invalideInput = false;
+        public double Power(double baseValue, int exponent)
+        {
+            if (baseValue.Equals(0.0) && exponent < 0)
+            {
+                return 0;
+            }
+            int absExponent = exponent;
+            if (exponent < 0)
+                absExponent = -exponent;
+            double result = PowerWithUnsignedExponent(baseValue, absExponent);
+            if (exponent < 0)
+            {
+                result = 1.0 / result;
+            }
+            return result;
+        }
+
+        private double PowerWithUnsignedExponent(double baseValue, int exponent)
+        {
+            if (exponent == 0)
+            {
+                return 1;
+            }
+            if (exponent == 1)
+            {
+                return baseValue;
+            }
+            double result = PowerWithUnsignedExponent(baseValue, exponent >> 1);
+            result *= result;
+            if ((exponent & 0x01) == 1)
+            {
+                result *= baseValue;
+            }
+            return result;
+        }
+    }
+}
